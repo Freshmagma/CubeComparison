32b45331397c21c9cd5b821e6dd9b1446e8f65e8
Freshmagma <89127780+Freshmagma@users.noreply.github.com>
2022-12-19 08:26:56 +0100

    Add files via upload

---
+++ ComparisonCubePrices/ComparisonCubePrices/ComparePrices.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace ComparisonCubePrices
+{
+    public class ComparePrices
+    {
+        private ChromeDriver webDriver;
+        public void Compare()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            webDriver = new ChromeDriver();
+
+            webDriver.Navigate().GoToUrl("https://www.gibz.ch");
+        }
+    }
+}
---
+++ ComparisonCubePrices/ComparisonCubePrices/Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using ComparisonCubePrices;
+
+Console.WriteLine("Hello, World!");
+ComparePrices Compare = new ComparePrices();
+Compare.Compare();
+
+Console.Clear();
+Console.WriteLine("Prices:");

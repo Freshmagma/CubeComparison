51c8fbde2c7a0b40ad81343a10fa7345bcbb49d7
Dominik Ott <dott@online.gibz.ch>
2022-12-19 08:38:02 +0100

    test

--- ComparisonCubePrices/ComparisonCubePrices/ComparePrices.cs
+++ ComparisonCubePrices/ComparisonCubePrices/ComparePrices.cs
@@ -9,12 +9,21 @@ namespace ComparisonCubePrices
     public class ComparePrices
     {
         private ChromeDriver webDriver;
-        public void Compare()
+
+        public void CompareCubeLess()
         {
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
             webDriver = new ChromeDriver();
 
-            webDriver.Navigate().GoToUrl("https://www.gibz.ch");
+            webDriver.Navigate().GoToUrl("https://www.cubeless.ch/?gclid=CjwKCAiAkfucBhBBEiwAFjbkr2Y_H7hj5ZguN7NkNAwZ8Ux6guLJ2SxX9E3JprdqXN1dSdMBKaV1txoCsQkQAvD_BwE");
+        }
+
+        public void CompareFabitasia()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            webDriver = new ChromeDriver();
+
+            webDriver.Navigate().GoToUrl("https://fabitasia.ch/");
         }
     }
 }

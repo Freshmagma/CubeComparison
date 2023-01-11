4dda89daa91e899796904fc493912e826e1069bc
Dominik Ott <dott@online.gibz.ch>
2023-01-11 19:59:30 +0100

    finished

--- ComparisonCubePrices/ComparisonCubePrices/ComparePrices.cs
+++ ComparisonCubePrices/ComparisonCubePrices/ComparePrices.cs
@@ -30,14 +30,27 @@ namespace ComparisonCubePrices
             act.MoveToElement(webDriver.FindElement(By.XPath("//div[@id='search_typeahead']/div/form/div/button"))).Click().Perform();
             webDriver.FindElement(By.XPath("//div[@id='search_typeahead']/div/form/div/input")).SendKeys(cubeName);
             act.MoveToElement(webDriver.FindElement(By.XPath("//div[@id='search_typeahead']/div/form/div/button"))).Click().Perform();
-            Thread.Sleep(5000);
-            webDriver.FindElement(By.XPath("//div[@id='content_col']/div/div/div/div[@class='searchresult-panel items']/a")).Click();
-            Thread.Sleep(5000);
+            try
+            {
+                webDriver.FindElement(By.XPath("//div[@id='content_col']/div/div/div/div[@class='searchresult-panel items']/a")).Click();
 
-            CubeCubeLess.CubePrice = webDriver.FindElement(By.XPath("//div[@id='pricebox']/div/p")).Text;
+                CubeCubeLess.CubePrice = webDriver.FindElement(By.XPath("//div[@id='pricebox']/div/p")).Text;
+
+                CubeCubeLess.CubeName = webDriver.FindElement(By.XPath("//div[@id='primary']/h1")).Text;
+                CubeCubeLess.ShippingTime = webDriver.FindElement(By.XPath("//div[@id='pricebox']/div/div/p[2]")).Text.Replace("Lieferzeit: ", "");
+
+                if (string.IsNullOrEmpty(CubeCubeLess.ShippingTime))
+                {
+                    CubeCubeLess.ShippingTime = "Nicht verfügbar";
+                }
+            }
+            catch
+            {
+                CubeCubeLess.CubeName = "Würfel nicht gefunden";
+            }
             CubeCubeLess.CubeSeller = "Cubeless";
-            CubeCubeLess.CubeName = cubeName;
 
+            webDriver.Quit();
             return CubeCubeLess;
         }
 
@@ -51,14 +64,33 @@ namespace ComparisonCubePrices
             webDriver.Navigate().GoToUrl("https://fabitasia.ch/");
             webDriver.FindElement(By.XPath("//form[@id='RemoteSearch1']/div/div/div/input")).SendKeys(cubeName);
             act.MoveToElement(webDriver.FindElement(By.XPath("//form[@id='RemoteSearch1']/div/div/div/button"))).Click().Perform();
-            Thread.Sleep(5000);
-            webDriver.FindElement(By.XPath("//div[@id='RemoteSearchResults']/div[@class='HotDealList']/table/tbody/tr/td/div/h3/a")).Click();
-            Thread.Sleep(5000);
+            try
+            {
+                webDriver.FindElement(By.XPath("//div[@id='RemoteSearchResults']/div[@class='HotDealList']/table/tbody/tr/td/div/h3/a")).Click();
 
-            CubeFabitasia.CubePrice = webDriver.FindElement(By.XPath("//div[@class='Price']/span/span[2]")).Text;
+                try
+                {
+                    CubeFabitasia.CubePrice = webDriver.FindElement(By.XPath("//div[@class='Price']/span/span[2]")).Text + " " + webDriver.FindElement(By.XPath("//div[@class='Price']/span/span[1]")).Text;
+                }
+                catch
+                {
+                    CubeFabitasia.CubePrice = "Unbekannt";
+                    CubeFabitasia.ShippingTime = "Nicht verfügbar";
+                }
+                CubeFabitasia.CubeName = webDriver.FindElement(By.XPath("//div[@class='InfoArea New']/h1")).Text;
+
+                if (CubeFabitasia.ShippingTime != "Nicht verfügbar")
+                {
+                    CubeFabitasia.ShippingTime = "Unbekannt";
+                }
+            }
+            catch
+            {
+                CubeFabitasia.CubeName = "Würfel nicht gefunden";
+            }
             CubeFabitasia.CubeSeller = "Fabitasia";
-            CubeFabitasia.CubeName = cubeName;
 
+            webDriver.Quit();
             return CubeFabitasia;
         }
 
@@ -72,14 +104,22 @@ namespace ComparisonCubePrices
             webDriver.Navigate().GoToUrl("https://www.thecubicle.com/");
             webDriver.FindElement(By.XPath("//input[@id='desktopSearchBar']")).SendKeys(cubeName);
             act.MoveToElement(webDriver.FindElement(By.XPath("//button[@class='search-bar--submit']"))).Click().Perform();
-            Thread.Sleep(5000);
-            webDriver.FindElement(By.XPath("//a[@class='product-grid-item']")).Click();
-            Thread.Sleep(5000);
+            try
+            {
+                webDriver.FindElement(By.XPath("//a[@class='product-grid-item']")).Click();
 
-            CubeTheCubicle.CubePrice = webDriver.FindElement(By.XPath("//span[@id='productPrice-product-template']/span[@class='visually-hidden']")).Text;
+                CubeTheCubicle.CubePrice = webDriver.FindElement(By.XPath("//span[@id='productPrice-product-template']/span[@class='visually-hidden']")).Text;
+
+                CubeTheCubicle.CubeName = webDriver.FindElement(By.XPath("//div[@id='ProductSection']/div/div/h1")).Text;
+                CubeTheCubicle.ShippingTime = "Unbekannt";
+            }
+            catch
+            {
+                CubeTheCubicle.CubeName = "Würfel nicht gefunden";
+            }
             CubeTheCubicle.CubeSeller = "TheCubicle";
-            CubeTheCubicle.CubeName = cubeName;
 
+            webDriver.Quit();
             return CubeTheCubicle;
         }
     }
